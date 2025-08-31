9540935952c8e567a55edd381962a664667af43c
Skyzopedia <masharun715@gmail.com>
2025-09-01 03:18:47 +0700

    Add files via upload

--- src/Client/Login/Cli.ts
+++ src/Client/Login/Cli.ts
@@ -10,26 +10,21 @@
 import type { Snake } from '../Snake.ts';
 import { Logger } from '../../Context/Logger.ts';
 import { Storages, Client, Raw, prompts, sysprc } from '../../platform.deno.ts';
-
 const onCancel = () => {
   Logger.info('Aborting prompt!!');
   sysprc.exit(1);
   return false;
 };
-
 export async function LoginWithCLI(snake: Snake): Promise<Raw.users.UserFull | undefined> {
   Logger.info('Initial login with CLI.');
-
   if (!snake._options.apiId) {
     Logger.debug('Api Id is missing.');
     await AskApiId(snake);
   }
-
   if (!snake._options.apiHash) {
     Logger.debug('Api Hash is missing.');
     await AskApiHash(snake);
   }
-
   Logger.debug('Creating client.');
   snake._client = new Client(
     snake._options.login.session as unknown as Storages.AbstractSession,
@@ -37,27 +32,54 @@ export async function LoginWithCLI(snake: Snake): Promise<Raw.users.UserFull | u
     snake._options.apiId,
     snake._options.clientOptions,
   );
-
   await (snake._options.login.session as unknown as Storages.AbstractSession).load();
-
   if (!(snake._options.login.session as unknown as Storages.AbstractSession).authKey) {
-    // Langsung login userbot tanpa opsi lain
-  const user = await snake._client.start({
-  phoneNumber: await AskPhoneNumber(),
-  password: AskPassword,
-  recoveryCode: AskRecoveryCode,
-  code: AskOTPCode,
-  firstname: AskFirstName,
-  lastname: AskLastName,
-});
-
-
+    if (snake._options.login.botToken) {
+      Logger.debug('Login using bot token.');
+      const user = await snake._client.start({
+        botToken: snake._options.login.botToken!,
+      });
+      Logger.log(
+        `${
+          snake._options.login.forceDotSession
+            ? `(${snake._options.login.sessionName}.session)`
+            : ``
+        } Loggined as: `,
+      );
+      await (snake._options.login.session as unknown as Storages.AbstractSession).save();
+      return user;
+    }
+    const loginAs = await AskLoginAs();
+    if (loginAs === 'undefined') return;
+    Logger.debug(`Login as: ${loginAs}`);
+    if (loginAs === 'bot') {
+      await AskBotToken(snake);
+      const user = await snake._client.start({
+        botToken: snake._options.login.botToken!,
+      });
+      Logger.log(
+        `${
+          snake._options.login.forceDotSession
+            ? `(${snake._options.login.sessionName}.session)`
+            : ``
+        } Loggined as: `,
+      );
+      await (snake._options.login.session as unknown as Storages.AbstractSession).save();
+      return user;
+    }
+    const user = await snake._client.start({
+      phoneNumber: AskPhoneNumber,
+      password: AskPassword,
+      recoveryCode: AskRecoveryCode,
+      code: AskOTPCode,
+      firstname: AskFirstName,
+      lastname: AskLastName,
+    });
     Logger.log(
       `${
         snake._options.login.forceDotSession ? `(${snake._options.login.sessionName}.session)` : ``
       } Loggined as: `,
     );
-
     await (snake._options.login.session as unknown as Storages.AbstractSession).save();
     return user;
   } else {
@@ -65,9 +87,6 @@ export async function LoginWithCLI(snake: Snake): Promise<Raw.users.UserFull | u
     return await snake.getMe();
   }
 }
-
-// ======================= PROMPTS =======================
-
 async function AskApiId(snake: Snake): Promise<string> {
   const { value } = await prompts(
     {
@@ -82,7 +101,6 @@ async function AskApiId(snake: Snake): Promise<string> {
   Logger.debug(`Setting up Api Id with: ${snake._options.apiId}.`);
   return String(value);
 }
-
 async function AskApiHash(snake: Snake): Promise<string> {
   const { value } = await prompts(
     {
@@ -97,7 +115,48 @@ async function AskApiHash(snake: Snake): Promise<string> {
   Logger.debug(`Setting up Api Hash with: ${snake._options.apiHash}.`);
   return String(value);
 }
-
+async function AskLoginAs(): Promise<string> {
+  const { value } = await prompts(
+    {
+      type: 'select',
+      name: 'value',
+      message: 'Login as?',
+      initial: 0,
+      choices: [
+        {
+          title: 'User',
+          description: 'Login as user using phone number.',
+          value: 'user',
+        },
+        {
+          title: 'Bot',
+          description: 'Login as bot using bot token from bot father.',
+          value: 'bot',
+        }
+      ],
+    },
+    { onCancel },
+  );
+  return String(value);
+}
+async function AskBotToken(snake: Snake): Promise<string> {
+  const { value } = await prompts(
+    {
+      type: 'text',
+      name: 'value',
+      message: 'Input your bot token!',
+      validate: (value: string) => (value ? true : false),
+    },
+    { onCancel },
+  );
+  snake._options.login.botToken = value;
+  const splitedBotToken = String(snake._options.login.botToken).split(':');
+  const securedBotToken = `${splitedBotToken[0]}:${splitedBotToken[1]
+    .slice(30)
+    .padStart(splitedBotToken[1].length, '*')}`;
+  Logger.debug(`Setting up Bot Token with: ${securedBotToken}.`);
+  return String(value);
+}
 async function AskPhoneNumber(): Promise<string> {
   const { value } = await prompts(
     {
@@ -110,7 +169,6 @@ async function AskPhoneNumber(): Promise<string> {
   );
   return String(value);
 }
-
 async function AskPassword(hint: string): Promise<string> {
   const { value } = await prompts(
     {
@@ -123,7 +181,6 @@ async function AskPassword(hint: string): Promise<string> {
   );
   return String(value);
 }
-
 async function AskRecoveryCode(): Promise<string> {
   const { value } = await prompts(
     {
@@ -136,7 +193,6 @@ async function AskRecoveryCode(): Promise<string> {
   );
   return String(value);
 }
-
 async function AskOTPCode(): Promise<string> {
   const { value } = await prompts(
     {
@@ -149,7 +205,6 @@ async function AskOTPCode(): Promise<string> {
   );
   return String(value);
 }
-
 async function AskFirstName(): Promise<string> {
   const { value } = await prompts(
     {
@@ -162,7 +217,6 @@ async function AskFirstName(): Promise<string> {
   );
   return String(value);
 }
-
 async function AskLastName(): Promise<string> {
   const { value } = await prompts(
     {
@@ -174,6 +228,4 @@ async function AskLastName(): Promise<string> {
     { onCancel },
   );
   return String(value);
-                                    }
-
-
+}
