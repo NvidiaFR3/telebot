506d4e0efea1c6bbdb2e41551928249f1451ccb5
NvidiaFR3 <pasukankrgtidur@gmail.com>
2025-10-20 17:10:43 +0700

    Update Cli.ts

--- src/Client/Login/Cli.ts
+++ src/Client/Login/Cli.ts
@@ -1,12 +1,3 @@
-/**
- * tgsnake - Telegram MTProto framework for nodejs.
- * Copyright (C) 2025 butthx <https://github.com/butthx>
- *
- * THIS FILE IS PART OF TGSNAKE
- *
- * tgsnake is a free software : you can redistribute it and/or modify
- * it under the terms of the MIT License as published.
- */
 import type { Snake } from '../Snake.ts';
 import { Logger } from '../../Context/Logger.ts';
 import { Storages, Client, Raw, prompts, sysprc } from '../../platform.deno.ts';
